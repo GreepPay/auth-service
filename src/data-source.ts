0a0db86a5fbcdfd51a24e19f03f9f9a629998008
Ayomide Akinola <drayfocus@gmail.com>
2025-04-03 05:35:27 +0100

    switched auth db

--- src/data-source.ts
+++ src/data-source.ts
@@ -14,6 +14,7 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  schema: "auth_service",
   synchronize: true,
   logging: true,
   ssl:
